fbb71eb0088252a75d71fdb74d9259ced920704e
sreelakshmiarjun27 <125890167+sreelakshmiarjun27@users.noreply.github.com>
2023-05-10 00:09:48 +0530

    Add files via upload
    Read the data from dataset and changed the name df to data1 dd to data2

--- ads assign3.py
+++ ads assign3.py
@@ -26,28 +26,156 @@ def read_data(fn):
     This function read data 
     return transposed value and original value
     """
-    df = pd.read_csv(fn)
-    dd = df.drop(columns=["Country Code","Indicator Name","Indicator Code"])
-    print(dd)
+    data1 = pd.read_csv(fn)
+    data2 = data1.drop(columns=["Country Code","Indicator Name","Indicator Code"])
+    print(data2)
     """
     replacing the nul vaules with 0
     """
-    dd = dd.replace(np.nan,0)
+    data2 = data2.replace(np.nan,0)
     u = ["Colombia","India"]
     """
-    selecting the countries from the list
+    selecting the countries from the list 
+    and check if dataframe contains specified value
     """
-    dd = dd["Country Name"].isin(u)
-    dd = df[dd]
+    data2 = data2["Country Name"].isin(u)
+    data2 = data1[data2]
     """
     dropping some columns
     """
-    dd = dd.drop(columns={"Country Code","Indicator Name","Indicator Code","Country Name"})
-    print(dd)
+    data2 = data2.drop(columns={"Country Code","Indicator Name","Indicator Code","Country Name"})
+    print(data2)
     """
     transposing 
     """
-    dt = np.transpose(dd)
+    dt = np.transpose(data2)
     dt = dt.reset_index()
     
-    
+    """
+    renaming the data with index as year
+    """
+    dt = dt.rename(columns={"index":"year"})
+    dt = dt.rename(columns={109:"INDIA",45:"COLOMBIA"})
+    dt = dt.dropna()
+    dt["COLOMBIA"] = pd.to_numeric(dt["COLOMBIA"])
+    dt["INDIA"] = pd.to_numeric(dt["INDIA"])
+    dt["year"] = pd.to_numeric(dt["year"])
+    return data1,dt;
+data,dt = read_data(r"C:\Users\BIJI PC\Desktop\data sci\ADS\assignment3\Forest sq.csv")
+sdata,sdt = read_data(r"C:\Users\BIJI PC\Desktop\data sci\ADS\assignment3\CO2.csv")
+
+"""
+optimising the curve
+"""
+param,cp = opt.curve_fit(curve,dt["year"],dt["INDIA"],p0=[4e8, 0.1])
+print(*param)
+""" 
+taking the error value
+"""
+sigma = np.sqrt(np.diag(cp))
+"""
+low and up values for error ranges
+"""
+low,up = err.err_ranges(dt["year"],curve,param,sigma)
+"""
+data fitting
+"""
+dt["fit"] = curve(dt["year"],*param)
+plt.plot(dt["year"],dt["INDIA"],label="data")
+plt.plot(dt["year"],dt["fit"],c="red",label="fit")
+"""
+plot the error ranges in the graph
+"""
+plt.fill_between(dt["year"],low,up,alpha=0.6)
+plt.title("INDIA(CO2 LIQUID)")
+plt.legend()
+plt.show()
+"""
+prediction of 2035
+""" 
+plt.figure()
+plt.title("PREDICTION OF CO2 LIQUID[2035]")
+pred_year = np.arange(1960,2035)
+pred_ind = curve(pred_year,*param)
+plt.plot(dt["year"],dt["INDIA"],label="data")
+plt.plot(pred_year,pred_ind,label="prediction")
+plt.legend()
+plt.show()
+
+"""
+prediction of co2 solid
+"""
+param,cp = opt.curve_fit(curve,sdt["year"],sdt["INDIA"],p0=[4e8, 0.1])
+sigma = np.sqrt(np.diag(cp))
+low,up = err.err_ranges(sdt["year"],curve,param,sigma)
+sdt["fit"] = curve(sdt["year"],*param)
+plt.plot(sdt["year"],sdt["INDIA"],label="data")
+plt.plot(sdt["year"],sdt["fit"],c="red",label="fit")
+plt.fill_between(sdt["year"],low,up,alpha=0.6)
+plt.title("INDIA(CO2 SOLID)")
+plt.legend()
+plt.show()
+plt.figure()
+plt.title("PREDICTION OF CO2 SOLID[2035]")
+pred_year = np.arange(1960,2035)
+pred_ind = curve(pred_year,*param)
+plt.plot(sdt["year"],sdt["INDIA"],label="data")
+plt.plot(pred_year,pred_ind,label="prediction")
+plt.legend()
+plt.show()
+"""
+clustering india vs colombia
+"""
+
+dt = dt.iloc[:,1:3]
+kmean = cluster.KMeans(n_clusters=2).fit(dt)
+label = kmean.labels_
+plt.scatter(dt["COLOMBIA"],dt["INDIA"],c=label,cmap="jet")
+plt.title("INDIA VS COLOMBIA(CO2 EMISSION LIQUID)")
+"""
+finding centroids
+"""
+c = kmean.cluster_centers_
+for s in range(2):
+    xc,yc = c[s,:]
+    plt.plot(xc,yc,"dk",markersize=13)
+plt.figure()
+plt.show()
+#def clustering(dt):
+"""
+    clustering solid vs liquid(india)
+    """
+#canada = pd.DataFrame()
+#print(canada)
+    #canada.dropna()
+#canada["forest sq"] = dt["INDIA"]
+#canada["CO2"] = sdt["INDIA"]
+#kmean = cluster.KMeans(n_clusters=2).fit(canada)
+#label = kmean.labels_
+#plt.scatter(canada["co2_liquid"],canada["co2_solid"],c=label,cmap="jet")
+#plt.title("CO2_LIQUID VS CO2_SOLID(INDIA)")
+"""
+finding centroids
+"""
+#c = kmean.cluster_centers_
+#for s in range(2):
+   # xc,yc = c[s,:]
+   # plt.plot(xc,yc,"dk",markersize=13)
+#plt.figure()
+#plt.show()
+
+
+sdt = sdt.iloc[:,1:3]
+kmean = cluster.KMeans(n_clusters=2).fit(sdt)
+label = kmean.labels_
+plt.scatter(sdt["COLOMBIA"],sdt["INDIA"],c=label,cmap="jet")
+plt.title("INDIA VS COLOMBIA(CO2 EMISSION LIQUID)")
+"""
+finding centroids
+"""
+c = kmean.cluster_centers_
+for s in range(2):
+    xc,yc = c[s,:]
+    plt.plot(xc,yc,"dk",markersize=13)
+plt.figure()
+plt.show()
